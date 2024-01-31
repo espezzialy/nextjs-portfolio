c90761f1787a606377534b32981e95a85bb7163b
Espezzialy Souza <espezzialy@outlook.com>
2024-01-31 16:18:38 -0300

    chore: adding settings.json for vscode

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,6 +1,10 @@
 /* eslint-disable react/no-unescaped-entities */
 export default function HeroSection() {
     return(
-      <h1 className="text-white">Hello I'm Espezzialy Souza</h1>
+      <section>
+       <h1 className="text-white">Hello I'm Espezzialy</h1>
+       <p className="text-[#ADB7BE"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit eligendi debitis alias excepturi odit esse sapiente porro tenetur numquam? Iste sint tenetur corporis obcaecati voluptate, praesentium in rem repudiandae quaerat.</p>
+      </section>
+
     )
 }
