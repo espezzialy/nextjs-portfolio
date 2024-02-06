21dc8db8bc2a1ed6b80d66235f19640d085f612d
Espezzialy Souza <espezzialy@outlook.com>
2024-02-06 17:03:25 -0300

    feat: finishing hero section design

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -4,12 +4,15 @@ import Image from "next/image"
 export default function HeroSection() {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12 my-4">
-        <div className="col-span-7 place-self-center place-items-center grid lg:place-items-start">
-          <h1 className="text-transparent text-6xl bg-clip-text bg-gradient-to-r from-blue-400 to-purple-600">
-            Hello, I'm Espezzialy
+      <div className="grid grid-cols-1 sm:grid-cols-12 my-4">
+        <div className="col-span-7 place-self-center place-items-center grid lg:place-items-start sm:text-left">
+          <h1 className="tex-white mb-4 sm:text-5xl lg:text-6xl">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-purple-600">
+              Hello, I'm {""}
+            </span>
+            Espezzialy
           </h1>
-          <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             {" "}
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit
             eligendi debitis alias excepturi odit esse sapiente porro tenetur
@@ -17,10 +20,10 @@ export default function HeroSection() {
             in rem repudiandae quaerat.
           </p>
           <div>
-            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bh-slate-200 text-black">
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-600 to-purple-600 bg-white hover:bg-slate-200 text-white">
               Hire Me
             </button>
-            <button className="px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3">
               Download CV
             </button>
           </div>
