d6e5f416b968e27dd5863b795e9bb9192094630b
Espezzialy Souza <espezzialy@outlook.com>
2024-02-07 15:19:41 -0300

    feat: starting email section and adding icons

---
+++ src/app/components/EmailSection.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/no-unescaped-entities */
+export default function EmailSection() {
+  return (
+    <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4">
+      <div>
+        <h5 className="text-xl font-bold text-white my-2 "> Let's Connect</h5>
+        <p className="text-[#ADB7BE] mb-4 max-2-md">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto
+          consectetur voluptatum, labore cupiditate, odit eaque similique quas
+          eos temporibus sunt beatae, quidem quae. Iste voluptas debitis, saepe
+          ut dicta ducimus.
+        </p>
+        <div className="socials flex flex-row gap-2"></div>
+      </div>
+    </section>
+  )
+}
