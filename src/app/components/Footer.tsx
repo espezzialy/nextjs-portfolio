61f7871afb99d3c0ecba438a5feec838dbf99d76
Espezzialy Souza <espezzialy@outlook.com>
2024-02-08 00:25:07 -0300

    feat: adding footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="bg-[#121212] z-10 footer border border-t-[#33353F] border-l-transparent border-r-transparent border-b-transparent ">
+      <div className="container p-12 flex justify-between">
+        <span className="text-2xl font-bold">LOGO</span>
+        <p className="text-slate-500 ">All rights reserved</p>
+      </div>
+    </footer>
+  )
+}
