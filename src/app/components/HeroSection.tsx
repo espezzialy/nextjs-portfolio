f92c787f28148794e4a831d320f2ece3d708a479
Espezzialy Souza <espezzialy@outlook.com>
2024-02-08 09:53:11 -0300

    feat: finishing achievements section and ajust for small screen

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -5,10 +5,10 @@ import { TypeAnimation } from "react-type-animation"
 /* eslint-disable react/no-unescaped-entities */
 export default function HeroSection() {
   return (
-    <section>
+    <section className="lg:py-16">
       <div className="grid grid-cols-1 sm:grid-cols-12 my-4">
-        <div className="col-span-7 place-self-center place-items-center grid lg:place-items-start sm:text-left">
-          <h1 className="tex-white mb-4 text-5xl lg:text-6xl font-extrabold">
+        <div className="col-span-8 place-self-center place-items-center grid lg:place-items-start sm:text-left">
+          <h1 className="tex-white mb-4 text-5xl lg:text-7xl lg:leading-normal justify-self-star font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-purple-600">
               Hello, I'm {""}
             </span>
@@ -44,7 +44,7 @@ export default function HeroSection() {
             </button>
           </div>
         </div>
-        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+        <div className="col-span-4 place-self-center mt-4 lg:mt-0">
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/hero-image.png"
