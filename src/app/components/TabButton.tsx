f0135aa6e75acf8225fa9148cd13626def24f940
Espezzialy Souza <espezzialy@outlook.com>
2024-02-08 12:07:38 -0300

    feat: adding animation to tab button about me

--- src/app/components/TabButton.tsx
+++ src/app/components/TabButton.tsx
@@ -1,3 +1,10 @@
+import { motion } from "framer-motion"
+
+const variants = {
+  default: { width: 0 },
+  active: { width: "calc(100% - 0.75rem" }
+}
+
 interface TabButtonProps {
   selectTab: () => void
   active: Boolean
@@ -5,15 +12,18 @@ interface TabButtonProps {
 }
 
 export default function TabButton(props: TabButtonProps) {
-  const buttonClasses = props.active
-    ? "text-white border-b border-blue-600"
-    : "text-[#ADB7BE]"
+  const buttonClasses = props.active ? "text-white" : "text-[#ADB7BE]"
   return (
     <div>
       <button onClick={props.selectTab}>
         <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
           {props.children}
         </p>
+        <motion.div
+          animate={props.active ? "active" : "default"}
+          variants={variants}
+          className="h-1 bg-blue-600 mt-1 mr-3 rounded-md"
+        ></motion.div>
       </button>
     </div>
   )
